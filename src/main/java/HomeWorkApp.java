d9b2bf10f665081df8147244843c3de3587e5b4e
pavel <pavelzubar@gmail.com>
2025-06-19 21:00:38 +0300

    Added new methods

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,5 +1,19 @@
 public class HomeWorkApp {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 3, b = 5;
+        if ((a+b) >= 0)
+            System.out.println("The sum is positive");
+        else
+            System.out.println("The sum is negative");
     }
 }
