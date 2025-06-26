f57ce72f4e14d22f12b0f847b5a5d0a3d195c898
pavel <pavelzubar@gmail.com>
2025-06-26 13:37:29 +0300

    Moved psvm method to separate class as required in the task

--- src/main/java/lesson_8/Employee.java
+++ src/main/java/lesson_8/Employee.java
@@ -4,20 +4,6 @@ public class Employee {
     private String fullName, position, email, phone;
     private int salary, age;
 
-    public static void main(String[] args) {
-        Employee[] empArr = new Employee[5];
-
-        empArr[0] = new Employee("Ivanov Ivan", "CEO", "iivan@gmail.com", "+375112223344", 3000, 45);
-        empArr[1] = new Employee("Vasiliev Vasiliy", "Frontend Dev", "vasvasiliy@gmail.com", "+375113334455", 2000, 46);
-        empArr[2] = new Employee("Vert Dider", "Backend Dev", "vedider@gmail.com", "+375113344555", 2200, 31);
-        empArr[3] = new Employee("Alisa Alisarova", "QA", "alisarovaalisa@gmail.com", "+375116667788", 1500, 36);
-        empArr[4] = new Employee("Evgniy Evgeniev", "HR", "evgenevgen@gmail.com", "+375111235476", 1000, 28);
-
-        for (Employee emp : empArr) {
-            if (emp.getAge() > 40) emp.Info();
-        }
-    }
-
     public Employee(String fullName, String position, String email, String phone, int salary, int age) {
         this.fullName = fullName;
         this.position = position;
---
+++ src/main/java/lesson_8/OutputEmployee.java
@@ -0,0 +1,17 @@
+package lesson_8;
+
+public class OutputEmployee {
+    public static void main(String[] args) {
+        Employee[] empArr = new Employee[5];
+
+        empArr[0] = new Employee("Ivanov Ivan", "CEO", "iivan@gmail.com", "+375112223344", 3000, 45);
+        empArr[1] = new Employee("Vasiliev Vasiliy", "Frontend Dev", "vasvasiliy@gmail.com", "+375113334455", 2000, 46);
+        empArr[2] = new Employee("Vert Dider", "Backend Dev", "vedider@gmail.com", "+375113344555", 2200, 31);
+        empArr[3] = new Employee("Alisa Alisarova", "QA", "alisarovaalisa@gmail.com", "+375116667788", 1500, 36);
+        empArr[4] = new Employee("Evgniy Evgeniev", "HR", "evgenevgen@gmail.com", "+375111235476", 1000, 28);
+
+        for (Employee emp : empArr) {
+            if (emp.getAge() > 40) emp.Info();
+        }
+    }
+}
